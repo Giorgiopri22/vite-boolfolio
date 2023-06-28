4e1e2a8bac516ccd1278e4439b9da1d20203bf78
Giorgio22 <giorgioprizzi@gmail.com>
2023-06-28 22:18:41 +0200

    pushino serale

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script >
+import axios from 'axios';
 
+  export default{
+    data(){
+      return{
+        baseUrl : 'http://127.0.0.1:8000',
+        projects: []
+      }
+    }, mounted(){
+        this.getProjects()
+    },methods:{
+      getProjects(){
+        axios.get(`${this.baseUrl}/api/projects` ).then(res=>{
+          this.projects = res.data.projects
+        })
+      }
+    }
+  }
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+  <div v-for="(projects,index) in projects " :key="index">
+    {{ projects.name }}
+    {{ projects.description }}
+    <img :src=" `${baseUrl}/storage/${projects.image}`" alt="">
+  </div>
 </template>
 
 <style lang="scss">
