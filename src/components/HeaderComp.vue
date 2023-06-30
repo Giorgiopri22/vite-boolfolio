6c84c329cc94bd3759f00b57609d78b802a15c97
Giorgio22 <giorgioprizzi@gmail.com>
2023-06-30 16:49:50 +0200

    Push della home

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -9,19 +9,24 @@ export default{
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">LOGO</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
+        <ul class="navbar-nav p-2">
             <li class="nav-item">
             <RouterLink class="nav-link active" aria-current="page" :to="{name: 'home' }">Home</RouterLink>
             </li>
             <li class="nav-item">
             <RouterLink class="nav-link" :to="{ name:'projects' }">Projects</RouterLink>
-            </li>
-            
+            </li>        
+        </ul>
+        <ul class="navbar-nav p-2">
+            <a href=""><li class="nav-item me-1"><i class="fa-brands fa-linkedin" style="color: #51441f;"></i></li></a>
+            <a href=""><li class="nav-item me-1"><i class="fa-brands fa-instagram" style="color: #51441f;"></i></li></a>
+            <a href="mailto:giorgioprizzi@gmail.com"><li class="nav-item me-1"><i class="fa-regular fa-envelope" style="color: #51441f;"></i></li></a>
+            <a href="https://github.com/Giorgiopri22"><li class="nav-item me-1"><i class="fa-brands fa-square-github" style="color: #51441f;"></i></li></a>
         </ul>
         </div>
     </div>
@@ -29,4 +34,6 @@ export default{
 </template>
 
 <style lang="scss" scoped>
+
+
 </style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,11 +1,95 @@
-<script>
-</script>
-
 <template>
-    <h1>ciao
-        
-    </h1>
-</template>
-
-<style lang="scss" scoped>
-</style>
+    <div class="matrix-background" ref="matrixBackground">
+      <div class="code-container">
+        <div v-for="">
+            <div v-for="(line, index) in codeLines" :key="index" class="code-line">
+            <div class="code-text">{{ line }}</div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        codeLines: [
+          "01001000",
+          "01100101",
+          "01101100",
+          "01101100",
+          "01101111",
+          "00100000",
+          "01010111",
+          "01101111",
+          "01110010",
+          "01101100",
+          "01100100",
+        ],
+        animationInterval: null,
+      };
+    },
+    mounted() {
+      this.startAnimation();
+      window.addEventListener("resize", this.handleResize);
+      this.handleResize();
+    },
+    beforeDestroy() {
+      this.stopAnimation();
+      window.removeEventListener("resize", this.handleResize);
+    },
+    methods: {
+      startAnimation() {
+        this.animationInterval = setInterval(() => {
+          this.codeLines.push(this.generateRandomCodeLine());
+          this.codeLines.shift();
+        }, 100);
+      },
+      stopAnimation() {
+        clearInterval(this.animationInterval);
+      },
+      generateRandomCodeLine() {
+        // Logica per generare una riga di codice casuale
+        return Math.random().toString(4).substring(3, 12);
+      },
+      handleResize() {
+        const matrixBackground = this.$refs.matrixBackground;
+        matrixBackground.style.height = `${window.innerHeight - 72}px`;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .matrix-background {
+    background-color: black;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+  }
+  
+  .code-container {
+    font-family: 'Courier New', monospace;
+    font-size: 16px;
+    color: green;
+    white-space: nowrap;
+    animation: scroll 5s linear infinite;
+  }
+  
+  .code-line {
+    padding: 2px 0;
+  }
+  
+  @keyframes scroll {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(100%);
+    }
+  }
+  </style>
+  
